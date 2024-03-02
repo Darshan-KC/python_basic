ddf078734e0355fd7a28a7465aac8d9c744a59b1
dkc1549 <dkc1549@gmail.com>
2024-03-02 20:29:44 +0545

    some change in problem 01

--- 01_snake_water_gun.py
+++ 01_snake_water_gun.py
@@ -1,4 +1,5 @@
-# Snake water gun game is a game which requires two players both have to select something from snake or water or gun. snake wins over water , gun win over snake and water wins over gun. ultimately the player with maximum points wins the game.
+# Snake water gun game is a game which requires two players both have to select something from snake or water or gun. 
+# snake wins over water , gun win over snake and water wins over gun. ultimately the player with maximum points wins the game.
 import random
 
 # To take computer input
