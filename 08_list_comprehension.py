82cb227bfc4e4d6363a74bbcdbdc83125b4abc0f
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-08 21:15:40 +0545

    08 list comprehension added

---
+++ 08_list_comprehension.py
@@ -0,0 +1,9 @@
+# Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+print(a)
+
+lst = [x for x in a if x%2==0]
+
+print(lst)
