506058cabea139dde471174f12c3ce163a6415d6
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-12 21:25:46 +0545

    12 primality function complete

--- 12_primality_function.py
+++ 12_primality_function.py
@@ -2,6 +2,21 @@
 import math
 
 def Prime(n :int ) ->bool:
-    
+    k=math.sqrt(n)
+    k=math.floor(k)
+    i=2
+    while i<=k:
+        if(n%i == 0):
+            return False
+        i+=1
     return True
+
+try:
+    n = int(input("Enter a number : "))
+    if(Prime(n)):
+        print("{} is a prime number.".format(n))
+    else:
+        print("{} is not a prime number.".format(n))
+except:
+    print("Invalid input!")
     
