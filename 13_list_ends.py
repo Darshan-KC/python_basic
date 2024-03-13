c52d864b27d40eb1e83fa73d45f56d26824adc03
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-13 21:39:43 +0545

    13 list ends added

---
+++ 13_list_ends.py
@@ -0,0 +1,1 @@
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
