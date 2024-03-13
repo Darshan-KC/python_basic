867869431d9c4aeaa74c15b81ad61ee1e709f709
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-13 21:44:43 +0545

    13 list ends complete

--- 13_list_ends.py
+++ 13_list_ends.py
@@ -1,1 +1,12 @@
 # Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+a = [5, 10, 15, 20, 25]
+
+lst = list()
+n = len(a)
+i = 0
+for x in range(n):
+    if(x==0 or x==(n-1)):
+        lst.append(a[x])
+        
+
+print(lst)
