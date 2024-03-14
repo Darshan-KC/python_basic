e53b3781ccb0d8c8d6d3ca767a1b0b149b715b73
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-14 21:08:32 +0545

    14 fibonacci added

---
+++ 13_fibonacci.py
@@ -0,0 +1,2 @@
+# Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. Take this opportunity to think about how you can use functions. Make sure to ask the user to enter the number of numbers in the sequence to generate.(Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+
