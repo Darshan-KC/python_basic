01e3e1306cb9820cd62298c127cfbffac09ce295
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-18 21:16:47 +0545

    18 binary search added

---
+++ 18_binary_search.py
@@ -0,0 +1,7 @@
+# Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) and another number. The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
+
+# Extras:
+
+# Use binary search.
+if __name__ == "__main__":
+    print("Namaste everyone")
