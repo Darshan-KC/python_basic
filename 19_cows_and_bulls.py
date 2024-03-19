b66ba9be7880731213a2f7562626c4e60feec5a4
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-19 20:09:08 +0545

    19 cow and bulls added

---
+++ 19_cows_and_bulls.py
@@ -0,0 +1,3 @@
+# Create a program that will play the “cows and bulls” game with the user. The game works like this:
+
+# Randomly generate a 4-digit number. Ask the user to guess a 4-digit number. For every digit that the user guessed correctly in the correct place, they have a “cow”. For every digit the user guessed correctly in the wrong place is a “bull.” Every time the user makes a guess, tell them how many “cows” and “bulls” they have. Once the user guesses the correct number, the game is over. Keep track of the number of guesses the user makes throughout teh game and tell the user at the end.
