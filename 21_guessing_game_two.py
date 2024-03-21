6b25c853ca1003106bb892a614b3c19f6ecc3637
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-21 21:25:11 +0545

    21 guessing game two added

---
+++ 21_guessing_game_two.py
@@ -0,0 +1,20 @@
+# This time, we’re going to do exactly the opposite. You, the user, will have in your head a number between 0 and 100. The program will guess a number, and you, the user, will say whether it is too high, too low, or your number.
+
+# At the end of this exchange, your program should print out how many guesses it took to get your number.
+
+# As the writer of this program, you will have to choose how your program will strategically guess. A naive strategy can be to simply start the guessing at 1, and keep going (2, 3, 4, etc.) until you hit the number. But that’s not an optimal guessing strategy. An alternate strategy might be to guess 50 (right in the middle of the range), and then increase / decrease by 1 as needed. After you’ve written the program, try to find the optimal strategy! (We’ll talk about what is the optimal one next week with the solution.)
+
+import random
+def welcome():
+    print("\t##########\t WELCOME TO THE GUESSING GAME \t##########")
+    print("\t******************************************************\n")
+    
+def compare():
+    pass
+
+def computer(min,max):
+    return random.randint(min,max)
+
+if __name__ == "__main__":
+    welcome()
+    compare()
