221876f5123ef7cacb8cd05d6245bb353e7846a0
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-24 20:39:20 +0545

    22 tic tac toe check added

---
+++ 22_tic_tac_toe_check.py
@@ -0,0 +1,10 @@
+# Today, we will simply focus on checking whether someone has WON a game of Tic Tac Toe, not worrying about how the moves were made.
+
+# If a game of Tic Tac Toe is represented as a list of lists, like so:
+
+# game = [[1, 2, 0],
+# 	[2, 1, 0],
+# 	[2, 1, 1]]
+# where a 0 means an empty square, a 1 means that player 1 put their token in that space, and a 2 means that player 2 put their token in that space.
+
+# Your task this week: given a 3 by 3 list of lists that represents a Tic Tac Toe game board, tell me whether anyone has won, and tell me which player won, if any. A Tic Tac Toe win is 3 in a row - either in a row, a column, or a diagonal. Don’t worry about the case where TWO people have won - assume that in every board there will only be one winner.
