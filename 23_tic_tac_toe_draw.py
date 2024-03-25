8ef18afb7d4e074bd0ffeb94ba3dcbb1c26671b5
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-25 21:30:02 +0545

    23 tic tac toe draw complete

--- 23_tic_tac_toe_draw.py
+++ 23_tic_tac_toe_draw.py
@@ -2,3 +2,28 @@
 # Notice how in the example I gave coordinates for where I want to move starting from (1, 1) instead of (0, 0). To people who don’t program, starting to count at 0 is a strange concept, so it is better for the user experience if the row counts and column counts start at 1. This is not required, but whichever way you choose to implement this, it should be explained to the player.
 # Ask the user to enter coordinates in the form “row,col” - a number, then a comma, then a number. Then you can use your Python skills to figure out which row and column they want their piece to be in.
 # Don’t worry about checking whether someone won the game, but if a player tries to put a piece in a game position where there already is another piece, do not allow the piece to go there.
+
+def draw(l):
+    n = 3
+    print(" ",end="")
+    print("--- "*n)
+    for i in range(n):
+        print("|",end="")
+        tem = " {} |"*n
+        print(tem.format(numtosym(l[i][0]),numtosym(l[i][1]),numtosym(l[i][2])))
+        print(" ",end="")
+        print("--- "*n)
+
+def numtosym(n):
+    if n == 0:
+        return " "
+    elif n == 1:
+        return "X"
+    else:
+        return "0"
+
+if __name__ == "__main__":
+    l = [[1, 2, 2],
+                    [2, 1, 0],
+                    [2, 1, 1]]
+    draw(l)
