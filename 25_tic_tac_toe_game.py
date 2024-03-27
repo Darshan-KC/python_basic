78f75abbbae069c8d4e9ed7ecc36d859a0221838
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-27 20:29:41 +0545

    25 tic tac toe game added

---
+++ 25_tic_tac_toe_game.py
@@ -0,0 +1,6 @@
+# built up a few components needed to build a Tic Tac Toe game in Python:
+
+# Draw the Tic Tac Toe game board
+# Checking whether a game board has a winner
+# Handle a player move from user input
+# The next step is to put all these three components together to make a two-player Tic Tac Toe game! Your challenge in this exercise is to use the functions from those previous exercises all together in the same program to make a two-player game that you can play with a friend
