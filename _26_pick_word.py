2b70350d4d849e29a8c497c212ef435de04991d5
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-28 20:30:32 +0545

    26 pick word added

---
+++ _26_pick_word.py
@@ -0,0 +1,1 @@
+# In this exercise, the task is to write a function that picks a random word from a list of words from the SOWPODS dictionary. Download this file and save it in the same directory as your Python code. This file is Peter Norvig’s compilation of the dictionary of words used in professional Scrabble tournaments. Each line in the file contains a single word.
