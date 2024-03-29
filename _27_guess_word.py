1cd44d03dc0865894bfb9f0a98cd07d59cbfcf59
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-29 21:42:41 +0545

    27 guess word added

---
+++ _27_guess_word.py
@@ -0,0 +1,23 @@
+# Let’s continue building Hangman. In the game of Hangman, a clue word is given by the program that the player has to guess, letter by letter. The player guesses one letter at a time until the entire word has been guessed. (In the actual game, the player can only guess 6 letters incorrectly before losing).
+
+# Let’s say the word the player has to guess is “EVAPORATE”.
+
+# >>> Welcome to Hangman!
+# _ _ _ _ _ _ _ _ _
+# >>> Guess your letter: S
+# Incorrect!
+# >>> Guess your letter: E
+# E _ _ _ _ _ _ _ E
+# ...
+
+import _26_pick_word as pick
+import random
+
+def intro():
+    pass
+
+def check():
+    pass
+
+if __name__ == "__main__":
+    intro()
