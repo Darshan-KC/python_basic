c0458583763f876111880a28b94576d00ce39804
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-31 20:46:50 +0545

    28 birthday dictionaries added

---
+++ _28_birthday_dictionaries.py
@@ -0,0 +1,9 @@
+# we will keep track of when our friend’s birthdays are, and be able to find that information based on their name. Create a dictionary (in your file) of names and birthdays. When you run your program it should ask the user to enter a name, and return the birthday of that person back to them. 
+# The interaction should look something like this:
+# Welcome to the birthday dictionary. We know the birthdays of:
+# Albert Einstein
+# Benjamin Franklin
+# Ada Lovelace
+# >>> Who's birthday do you want to look up?
+# Benjamin Franklin
+# >>> Benjamin Franklin's birthday is 01/17/1706.
