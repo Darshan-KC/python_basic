6be67bf4700512c38097ef2f1b14affc6ec67dec
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-03-31 21:04:30 +0545

    28 birthday dictionaries complete

--- _28_birthday_dictionaries.py
+++ _28_birthday_dictionaries.py
@@ -7,3 +7,22 @@
 # >>> Who's birthday do you want to look up?
 # Benjamin Franklin
 # >>> Benjamin Franklin's birthday is 01/17/1706.
+
+birthday = {
+    "ram" : "2000/01/03",
+    "hari" : "1999/08/16",
+    "sita" : "2002/09/22",
+    "gita" : "2003/09/23",
+}
+
+print("Welcome to the birthday dictionary. We know the birthdays of : ")
+for key in birthday:
+    print(key.capitalize())
+    
+name = input("Who's birthday do you want to look up? ")
+name = name.lower()
+result = birthday.get(name)
+if(result == None):
+    print("\n!!!Enter the name that is given!!!\n")
+else:
+    print("{}'s birthday is {}.".format(name.capitalize(),result))
