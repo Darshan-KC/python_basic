54ec479d681655b90033cdaaa560e7f33286fc5a
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-01 20:01:02 +0545

    29 birthday json added

---
+++ _29_birthday_json.py
@@ -0,0 +1,3 @@
+# In this exercise, modify your program from Part 1 to load the birthday dictionary from a JSON file on disk, rather than having the dictionary defined in the program.
+
+# Bonus: Ask the user for another scientist’s name and birthday to add to the dictionary, and update the JSON file you have on disk with the scientist’s name. If you run the program multiple times and keep adding new names, your JSON file should keep getting bigger and bigger.
