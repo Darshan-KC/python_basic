099e628a2a722dbe37983953c4a1c29c0ed2fd4f
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-04 20:56:36 +0545

    30 birthday month added

---
+++ _30_birthday_month.py
@@ -0,0 +1,9 @@
+# In the previous exercise we saved information about famous scientists’ names and birthdays to disk. In this exercise, load that JSON file from disk, extract the months of all the birthdays, and count how many scientists have a birthday in each month.
+
+# Your program should output something like:
+
+# {
+# 	"May": 3,
+# 	"November": 2,
+# 	"December": 1
+# }
