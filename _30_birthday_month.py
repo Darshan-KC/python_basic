a1e87da15e7e479860281a865eb9109f545a818f
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-05 22:44:26 +0545

    30 birthday month complete

--- _30_birthday_month.py
+++ _30_birthday_month.py
@@ -12,9 +12,34 @@
 from collections import Counter
 import json
 
+def numtomonth(num):
+    month = {
+        1:"January",2:"February",3:"March",4:"April",5: "May",6: "June",7: "July",8: "August",9: "September", 10:"October",11: "November",12: "December"
+    }
+    return month.get(num)
+
 def readFromFile(file_name):
     with open(file_name,'r') as f:
         data = json.load(f)
+    return data
 
 if __name__ == "__main__":
-    pass
+    data = readFromFile("birthday.json")
+    
+    birthdays = dict()
+    result = dict()
+    # birthday count 
+    for i in range(1,13):
+        count = 0
+        for x,y in data.items():
+            dob = y.split('/')
+            if(i == int(dob[1])):
+                count +=1
+        birthdays[i] = count
+    for x,y in birthdays.items():
+        if y > 0:
+            index = numtomonth(x)
+            result[index] = y
+    
+    # print result
+    print(result)
