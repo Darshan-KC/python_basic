7a9f917a1df0ba6ce0cb81d4e161855fe6b0d11f
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-06 20:44:45 +0545

    31 birthday plots added

---
+++ _31_birthday_plots.py
@@ -0,0 +1,3 @@
+# In the previous exercise we counted how many birthdays there are in each month in our dictionary of birthdays.
+
+# In this exercise, use the bokeh Python library to plot a histogram of which months the scientists have birthdays in! Because it would take a long time for you to input the months of various scientists, you can use my scientist birthday JSON file. Just parse out the months (if you don’t know how, I suggest looking at the previous exercise or its solution) and draw your histogram.
