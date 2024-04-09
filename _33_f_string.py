b6928cc9d7edcb40e578ab7a28c02ac40699104b
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-09 19:43:27 +0545

    33 f string added

---
+++ _33_f_string.py
@@ -0,0 +1,1 @@
+# Implement the same exercise as Exercise 1 (Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old), except use f-strings instead of the + operator to print the resulting output message.
