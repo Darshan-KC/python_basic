9aa0a36a1aa69da84e1673ea8f7c4a964eb4fe08
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-16 21:04:01 +0545

    35 decode a web page two added

---
+++ _35_decode_a_web_page_two.py
@@ -0,0 +1,7 @@
+# Using the requests and BeautifulSoup Python libraries, print to the screen the full text of the article on this website: http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture.
+
+# The article is long, so it is split up between 4 pages. Your task is to print out the text to the screen so that you can read the full article without having to click any buttons.
+
+# (Hint: The post here describes in detail how to use the BeautifulSoup and requests libraries through the solution of the exercise posted here.)
+
+# This will just print the full text of the article to the screen. It will not make it easy to read
