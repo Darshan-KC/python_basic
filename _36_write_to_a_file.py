0636ab8e2bcf719508af0a13ff996d30712674ad
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-19 21:46:50 +0545

    36 write to a file complete

--- _36_write_to_a_file.py
+++ _36_write_to_a_file.py
@@ -3,3 +3,30 @@
 # Extras:
 
 # Ask the user to specify the name of the output file that will be saved.
+
+import requests
+from bs4 import BeautifulSoup
+
+def scraping_and_writing(url,file_name):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text,"html.parser")
+        paragraphs = soup.find_all('p')
+        data = [x.get_text() for x in paragraphs]
+        data_text = "\n\n".join(data)
+        with open(file_name,"w", encoding="utf-8") as f:
+            f.write(data_text)
+        print("Successfully scrap and write data into the file ",file_name)
+    else:
+        print("Failed to retrieve data from the URL. Status code:", response.status_code)
+        
+if __name__ == "__main__":
+    base_url = "https://www.nytimes.com/"
+    
+    file_name = input("Enter file name (e.g., abc.txt): ")
+    
+    if file_name.endswith((".txt", ".json", ".html", ".htm")):
+        scraping_and_writing(base_url, file_name)
+    else:
+        print("Invalid file format. Please provide a file name with .txt, .json, .html, or .htm extension.")
+        
