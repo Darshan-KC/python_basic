09e8795fddce102dd263026d06e861f6efc60e2f
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-19 21:19:55 +0545

    36 write to a file added

---
+++ _36_write_to_a_file.py
@@ -0,0 +1,5 @@
+# Take the code from the How To Decode A Website exercise and instead of printing the results to a screen, write the results to a txt file. In your code, just make up a name for the file you are saving to.
+
+# Extras:
+
+# Ask the user to specify the name of the output file that will be saved.
