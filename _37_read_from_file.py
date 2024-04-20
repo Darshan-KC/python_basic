5590a83385e6c6a8629166e9d933218c99e09456
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-20 20:31:37 +0545

    37 read from file added

---
+++ _37_read_from_file.py
@@ -0,0 +1,5 @@
+# Given a .txt file that has a list of a bunch of names, count how many of each name there are in the file, and print out the results to the screen. I have a .txt file for you, if you want to use it!
+
+# Extra:
+
+# Instead of using the .txt file from above (or instead of, if you want the challenge), take this .txt file, and count how many of each “category” of each image there are.
