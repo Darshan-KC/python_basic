03886c8071fc05a28f7ade355b2c8625a7efd96d
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-04-21 20:37:04 +0545

    38 file overlapping added

---
+++ _38_file_overlapping.py
@@ -0,0 +1,1 @@
+# Given two .txt files that have lists of numbers in them, find the numbers that are overlapping. One .txt file has a list of all prime numbers under 1000, and the other .txt file has a list of happy numbers up to 1000.
